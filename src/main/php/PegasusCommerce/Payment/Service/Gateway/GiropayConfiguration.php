9d0b9519d67914e8f8a4cb05753d2bae3e7706ad
Julian Haupt <julian.haupt@spielmeister.com>
2014-08-20 19:36:51 +0200

    Implemented GiropayConfiguration

--- src/main/php/PegasusCommerce/Payment/Service/Gateway/GiropayConfiguration.php
+++ src/main/php/PegasusCommerce/Payment/Service/Gateway/GiropayConfiguration.php
@@ -1,4 +1,25 @@
 <?php
-interface GiropayConfiguration extends PaymentGatewayConfiguration {
+namespace PegasusCommerce\Core\Payment\Service;
+
+use PegasusCommerce\Common\Payment\PaymentGatewayType;
+use PegasusCommerce\Common\Payment\Service\failureReportingThreshold;
+use PegasusCommerce\Common\Payment\Service\PaymentGatewayConfiguration;
+use PegasusCommerce\Vendor\Giropay\Service\Payment\GiropayPaymentGatewayType;
+
+interface GiropayGatewayConfiguration extends PaymentGatewayConfiguration {
+    /**
+     * @return string
+     */
+    public function getSecret();
+
+    /**
+     * @return string
+     */
+    public function getMerchantId();
+
+    /**
+     * @return string
+     */
+    public function getProjectId();
 
 }
