d717e65a717d61a907519d0131d31f7fc655e84d
Julian Haupt <julian.haupt@spielmeister.com>
2014-08-26 00:14:22 +0200

    First partial implementation of the GiropayResponseGenerator

--- src/main/php/PegasusCommerce/Vendor/Giropay/Service/Payment/GiropayResponseGenerator.php
+++ src/main/php/PegasusCommerce/Vendor/Giropay/Service/Payment/GiropayResponseGenerator.php
@@ -3,11 +3,16 @@ namespace PegasusCommerce\Vendor\Giropay\Service\Payment;
 
 use Guzzle\Http\Message\Response;
 use PegasusCommerce\Common\Payment\Dto\PaymentResponseDTO;
+use PegasusCommerce\Vendor\Giropay\Service\Payment\Message\GiropayRequest;
 
 interface GiropayResponseGenerator {
     /**
      * @return PaymentResponseDTO
      */
-    public function buildResponse(Response $httpResponse);
+    function buildResponse(Response $httpResponse, GiropayRequest $paymentRequest);
 
+    /**
+     * @return String
+     */
+    function getSecret();
 }
--- src/main/php/PegasusCommerce/Vendor/Giropay/Service/Payment/GiropayResponseGeneratorImpl.php
+++ src/main/php/PegasusCommerce/Vendor/Giropay/Service/Payment/GiropayResponseGeneratorImpl.php
@@ -3,14 +3,65 @@ namespace PegasusCommerce\Vendor\Giropay\Service\Payment;
 
 use Guzzle\Http\Message\Response;
 use PegasusCommerce\Common\Payment\Dto\PaymentResponseDTO;
+use PegasusCommerce\Common\Payment\PaymentGatewayType;
+use PegasusCommerce\Common\Payment\PaymentType;
+use PegasusCommerce\Vendor\Giropay\Service\Payment\Message\GiropayRequest;
+use PegasusCommerce\Vendor\Giropay\Service\Payment\Type\GiropayMethodType;
 
 class GiropayResponseGeneratorImpl implements GiropayResponseGenerator {
 
     /**
+     * @param Response $httpResponse
+     * @return PaymentResponseDTO
+     * @throws RuntimeException
+     */
+    protected function buildTransactionStartResponse(Response $httpResponse) {
+        $response = new PaymentResponseDTO(
+            PaymentType::$BANK_ACCOUNT,
+            GiropayPaymentGatewayType::$GIROPAY
+        );
+
+        $response->rawResponse(
+          $httpResponse->getBody(true)
+        );
+        $data = $httpResponse->json();
+
+        print_r($httpResponse->getMessage());
+
+   //     print_r($httpResponse->getHeaders());
+
+ //       print_r($response->getRawResponse());
+//        $response->valid();
+
+        return $response;
+
+    }
+
+    /**
      * @return PaymentResponseDTO
      */
-    public function buildResponse(Response $httpResponse)
+    public function buildResponse(Response $httpResponse, GiropayRequest $paymentRequest)
     {
-        // TODO: Implement buildResponse() method.
+        $giropayResponse = null;
+
+        if(GiropayMethodType::$TRANSACTION_START->equals($paymentRequest->getMethodType())) {
+            $giropayResponse = $this->buildTransactionStartResponse($httpResponse);
+        } else {
+            throw new InvalidArgumentException("Method type not supported: " . $paymentRequest->getMethodType()->getFriendlyType());
+        }
+
+        return $giropayResponse;
+    }
+
+    public function setSecret($secret) {
+
+    }
+
+    /**
+     * @return String
+     */
+    public function getSecret()
+    {
+        // TODO: Implement getSecret() method.
     }
 }
