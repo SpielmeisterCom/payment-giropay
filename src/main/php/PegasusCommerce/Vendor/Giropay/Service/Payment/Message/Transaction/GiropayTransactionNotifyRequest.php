264c9bb3a5195eabd46fa7cf73f028b3831a80b3
Julian Haupt <jh@spielmeister.com>
2014-08-28 18:55:37 +0200

    set the correct method type: GiropayMethodType::$TRANSACTION_NOTIFY

--- src/main/php/PegasusCommerce/Vendor/Giropay/Service/Payment/Message/Transaction/GiropayTransactionNotifyRequest.php
+++ src/main/php/PegasusCommerce/Vendor/Giropay/Service/Payment/Message/Transaction/GiropayTransactionNotifyRequest.php
@@ -1,11 +1,15 @@
 <?php
 namespace PegasusCommerce\Vendor\Giropay\Service\Payment\Message\Transaction;
 
+use PegasusCommerce\Vendor\Giropay\Service\Payment\Message\GiropayRequest;
 use PegasusCommerce\Vendor\Giropay\Service\Payment\Message\GiropayResponse;
+use PegasusCommerce\Vendor\Giropay\Service\Payment\Type\GiropayMethodType;
 
 /**
  * Dummy object that will be used to generate the GiropayTransactionNotifyResponse
  */
 class GiropayTransactionNotifyRequest extends GiropayRequest {
-
+    public function __construct() {
+        $this->setMethodType(GiropayMethodType::$TRANSACTION_NOTIFY);
+    }
 }
