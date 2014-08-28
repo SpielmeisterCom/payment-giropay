01a3b40d97fd9b13f57011fb3bd4758436169117
Julian Haupt <jh@spielmeister.com>
2014-08-28 18:47:31 +0200

    fixed wrong class name in GiropayTransactionNotifyRequest

--- src/main/php/PegasusCommerce/Vendor/Giropay/Service/Payment/Message/Transaction/GiropayTransactionNotifyRequest.php
+++ src/main/php/PegasusCommerce/Vendor/Giropay/Service/Payment/Message/Transaction/GiropayTransactionNotifyRequest.php
@@ -6,6 +6,6 @@ use PegasusCommerce\Vendor\Giropay\Service\Payment\Message\GiropayResponse;
 /**
  * Dummy object that will be used to generate the GiropayTransactionNotifyResponse
  */
-class GiropayTransactionNotifyResponse extends GiropayRequest {
+class GiropayTransactionNotifyRequest extends GiropayRequest {
 
 }
