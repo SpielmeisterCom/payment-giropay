4da58d6555e249b3198d69de2365217e49bb89cf
Julian Haupt <julian.haupt@spielmeister.com>
2014-09-04 01:29:34 +0200

    fiexed wrong imports

--- src/main/php/PegasusCommerce/Payment/Service/Gateway/GiropayReportingServiceImpl.php
+++ src/main/php/PegasusCommerce/Payment/Service/Gateway/GiropayReportingServiceImpl.php
@@ -1,18 +1,13 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: julian
- * Date: 27.08.14
- * Time: 15:19
- */
-
 namespace PegasusCommerce\Payment\Service\Gateway;
 
 use PegasusCommerce\Common\Payment\Dto\PaymentRequestDTO;
+use PegasusCommerce\Common\Payment\Dto\PaymentResponseDTO;
+use PegasusCommerce\Common\Payment\PaymentType;
 use PegasusCommerce\Common\Payment\Service\PaymentGatewayReportingService;
-use PegasusCommerce\Common\Payment\Service\PaymentResponseDTO;
 use PegasusCommerce\Core\Payment\Service\Exception\PaymentException;
 use PegasusCommerce\Vendor\Giropay\Service\Payment\GiropayConstants;
+use PegasusCommerce\Vendor\Giropay\Service\Payment\GiropayPaymentGatewayType;
 use PegasusCommerce\Vendor\Giropay\Service\Payment\Message\Transaction\GiropayTransactionStatusRequest;
 
 class GiropayReportingServiceImpl implements PaymentGatewayReportingService {
