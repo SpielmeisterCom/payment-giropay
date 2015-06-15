8d76339bd12e7a73049c95283a9682e40117312a
Julian Haupt <julian.haupt@abfallscout.de>
2015-06-15 13:42:54 +0200

    added Giropay Controller stub for redirect and notify routes

---
+++ src/PHPCommerce/GiropayPaymentBundle/Controller/GiropayController.php
@@ -0,0 +1,20 @@
+<?php
+namespace PHPCommerce\GiropayPaymentBundle\Controller;
+
+class GiropayController {
+    /**
+     * This is the url where the buyer will be sent after the payment was processed
+     */
+    public function redirectAction() {
+        echo "REDIRECT FOR GIROPAY";
+        exit;
+    }
+
+    /**
+     * This endpoint will receive the notification after the payment has been processed
+     */
+    public function notifyAction() {
+        echo "NOTIFY FOR GIROPAY";
+        exit;
+    }
+}
