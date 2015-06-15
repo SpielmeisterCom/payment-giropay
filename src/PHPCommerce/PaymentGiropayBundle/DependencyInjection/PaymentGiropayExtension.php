1d925c00430b0810a7fefa51a21c1a87ad45afa0
Julian Haupt <julian.haupt@abfallscout.de>
2015-06-15 17:49:06 +0200

    changed bundle name

--- src/PHPCommerce/PaymentGiropayBundle/DependencyInjection/PaymentGiropayExtension.php
+++ src/PHPCommerce/PaymentGiropayBundle/DependencyInjection/PaymentGiropayExtension.php
@@ -6,7 +6,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
 
-class PHPCommercePaymentGiropayExtension extends Extension
+class PaymentGiropayExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
--- src/PHPCommerce/PaymentGiropayBundle/PaymentGiropayBundle.php
+++ src/PHPCommerce/PaymentGiropayBundle/PaymentGiropayBundle.php
@@ -3,6 +3,6 @@ namespace PHPCommerce\PaymentGiropayBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class PHPCommercePaymentGiropayBundle extends Bundle
+class PaymentGiropayBundle extends Bundle
 {
 }
